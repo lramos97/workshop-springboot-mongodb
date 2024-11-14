2c38bc2c3c5eb2f0648eaa6c38aa7c0ec1c6b3c5
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-14 09:01:36 -0300

    Project created

---
+++ src/main/java/com/ramoslarissa/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.ramoslarissa.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
