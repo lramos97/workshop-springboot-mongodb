1bb101ecbb015c6c8291e8276f91eada54b1c836
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-14 11:21:30 -0300

    Implementing DTO standard

--- src/main/java/com/ramoslarissa/workshopmongo/resource/UserResourse.java
+++ src/main/java/com/ramoslarissa/workshopmongo/resource/UserResourse.java
@@ -1,6 +1,7 @@
 package com.ramoslarissa.workshopmongo.resource;
 
 import com.ramoslarissa.workshopmongo.domain.User;
+import com.ramoslarissa.workshopmongo.dto.UserDTO;
 import com.ramoslarissa.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -20,9 +21,10 @@ public class UserResourse {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
 
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
