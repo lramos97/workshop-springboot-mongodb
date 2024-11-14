ca122a234c89e07f7b1f4a0eec526d3b03b84e5f
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-14 15:07:48 -0300

    Update user with PUT

--- src/main/java/com/ramoslarissa/workshopmongo/resource/UserResourse.java
+++ src/main/java/com/ramoslarissa/workshopmongo/resource/UserResourse.java
@@ -49,4 +49,11 @@ public class UserResourse {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO((objDto));
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 }
