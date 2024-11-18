900ffa635a3864fde47fccd49e7a584eb63f59b1
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-18 14:32:22 -0300

    "Creating entity Post with nested User"

---
+++ src/main/java/com/ramoslarissa/workshopmongo/domain/Post.java
@@ -0,0 +1,24 @@
+package com.ramoslarissa.workshopmongo.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Document
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+}
---
+++ src/main/java/com/ramoslarissa/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.ramoslarissa.workshopmongo.repositories;
+
+import com.ramoslarissa.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
