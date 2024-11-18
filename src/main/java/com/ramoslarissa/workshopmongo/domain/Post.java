fd155373b1bf0a2b37964a8e35f90ce3ca8c289d
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-18 16:15:17 -0300

    Referencing user posts

--- src/main/java/com/ramoslarissa/workshopmongo/domain/Post.java
+++ src/main/java/com/ramoslarissa/workshopmongo/domain/Post.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "id")
 public class Post implements Serializable {
 
     @Id
--- src/main/java/com/ramoslarissa/workshopmongo/domain/User.java
+++ src/main/java/com/ramoslarissa/workshopmongo/domain/User.java
@@ -3,20 +3,31 @@ package com.ramoslarissa.workshopmongo.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(of="id")
+@EqualsAndHashCode(of = "id")
 public class User implements Serializable {
 
     @Id
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true) //para carregar os posts somente quando forem chamados, não toda vez que acessar o usuário
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
