01a1899ca85240b927a33c8248e6597b11be069c
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-18 17:37:06 -0300

    Simple query with @Query

--- src/main/java/com/ramoslarissa/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/ramoslarissa/workshopmongo/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.ramoslarissa.workshopmongo.repositories;
 
 import com.ramoslarissa.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,10 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    List<Post> findByTitleContainingIgnoreCase(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);//query methods spring-mongoDB. IgnoreCase para ignorar maiusculas e minusculas
+
+
 }
