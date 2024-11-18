96e04d25d2beb0ee46ed1e445d1194f522901b40
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-18 16:53:26 -0300

    Adding comments to posts

--- src/main/java/com/ramoslarissa/workshopmongo/domain/Post.java
+++ src/main/java/com/ramoslarissa/workshopmongo/domain/Post.java
@@ -1,17 +1,19 @@
 package com.ramoslarissa.workshopmongo.domain;
 
 import com.ramoslarissa.workshopmongo.dto.AuthorDTO;
+import com.ramoslarissa.workshopmongo.dto.CommentDTO;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Post implements Serializable {
@@ -22,4 +24,14 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 }
---
+++ src/main/java/com/ramoslarissa/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,20 @@
+package com.ramoslarissa.workshopmongo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO implements Serializable {
+
+    private String text;
+    private Date date;
+    private AuthorDTO author;
+}
