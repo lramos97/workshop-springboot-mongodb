2314d4b822ae14d13e0aeaa901747074113aa591
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-18 17:19:08 -0300

    Simple query with query methods

--- src/main/java/com/ramoslarissa/workshopmongo/resource/PostResourse.java
+++ src/main/java/com/ramoslarissa/workshopmongo/resource/PostResourse.java
@@ -1,10 +1,13 @@
 package com.ramoslarissa.workshopmongo.resource;
 
 import com.ramoslarissa.workshopmongo.domain.Post;
+import com.ramoslarissa.workshopmongo.resource.util.URL;
 import com.ramoslarissa.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,5 +22,13 @@ public class PostResourse {
 
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/ramoslarissa/workshopmongo/services/PostService.java
+++ src/main/java/com/ramoslarissa/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.ramoslarissa.workshopmongo.services.exception.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +20,7 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
+    }
 }
